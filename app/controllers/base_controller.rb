aa9939ff5581bbdb913519abfe0da3bde918e8a3
Ed Schmalzle <Jonas714@gmail.com>
2008-09-24 23:18:14 -0400

    cleaned up base controller [#2]

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -5,42 +5,58 @@ class BaseController < ApplicationController
   end
   
   def select_location
+    @page_title = "Select Location"
+    
+    # Redirect to next step if the location is set in the session
     if( session[ :location ] != nil ) then
       redirect_to :action => "select_sub_location"
     end
-    @page_title = "Select Location"
+    
     @locations = Location.find :all
   end
   
   def select_sub_location
+    @page_title = "Select Sub-Location"    
+    
+    # Put location into session
     if( session[ :location ] == nil ) then
       location = Location.find( params[:id] )
       session[ :location ] = location
     end
-    @sub_locations = SubLocation.find( :conditions => "location_id = '#{session[ :location ].id}'" )
+    
+    @sub_locations = session[ :location ].sub_locations
+    
+    # Redirect to next step if sub location is already OR if there are no sub-locations for this location
     if( session[ :sub_location ] != nil or @sub_locations.empty? ) then
       redirect_to :action => "select_category"
-    end    
-    @page_title = "Select Sub-Location"
+    end      
   end
   
-  def select_category
+  def select_category    
+    @page_title = "Select Category"
+    
+    # Redirect to next step if the category is set in the session
     if( session[ :category ] != nil ) then
       redirect_to :action => "select_sub_category"
     end
-    @page_title = "Select Category"
-    sub_location = SubLocation.find( params[ :id ] )
-    session[ :sub_location ] = SubLocation.find( sub_location.id )
-    # TODO: Categories retrieved in select_category action should be location specific
-    @categories = Category.find :all
+    
+    # Determine if we're coming from 'select_location' or 'select_sub_location'
+    if( params[ :id ] == nil ) then
+      # Populate the list of categories based on the location
+      @categories = Category.find :all
+    else
+      sub_location = SubLocation.find( params[ :id ] )
+      session[ :sub_location ] = SubLocation.find( sub_location.id )
+      @categories = Category.find :all
+    end    
   end
   
   def select_sub_category
+    @page_title = "Select Sub-Category"
     # TODO: Redirect to search action from select_sub_category action if there are no sub-categories
     if( session[ :sub_category ] != nil ) then
       redirect_to :action => "search"
     end
-    @page_title = "Select Sub-Category"
     category = Category.find( params[:id] )
     @sub_categories = Category.find( category.id ).sub_categories
   end
