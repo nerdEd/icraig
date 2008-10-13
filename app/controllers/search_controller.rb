86365ba3c879b5a5030872cff458857523fd17d6
Ed Schmalzle <Jonas714@gmail.com>
2008-10-13 13:08:55 -0400

    implemented basic search screen [#20]

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,19 @@ class SearchController < ApplicationController
   end
   
   def search
-    puts params
+    primary_location = session[ :location ]
+    sub_location = session[ :sub_location ]
+    primary_category = session[ :category ]
+    sub_category = session[ :sub_category ]
+    if( sub_location == nil ) then
+      url = primary_location.url
+    else 
+      url = sub_location.url
+    end
+    url = url + sub_category.code
+    search_terms = params[ :search ][ :terms ]
+    query = { :query => search_terms }
+    url = url + "?" + query.to_query
+    redirect_to url    
   end
 end
