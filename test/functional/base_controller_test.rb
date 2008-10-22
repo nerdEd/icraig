e07ab5d6f1f979ace318c2b70a26cab9faf0bfdc
Ed Schmalzle <Jonas714@gmail.com>
2008-10-21 22:08:50 -0400

    update base_controller functional test

--- test/functional/base_controller_test.rb
+++ test/functional/base_controller_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class BaseControllerTest < ActionController::TestCase
+  
+  def setup 
+    @controller = BaseController.new 
+    @request = ActionController::TestRequest.new 
+    @response = ActionController::TestResponse.new 
+  end 
+  
+  def test_index
+    get :index 
+    assert_response :success     
+  end
+  
   # Replace this with your real tests.
   def test_truth
     assert true
