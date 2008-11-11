22ca0435ebd784b704577fd0a63f207a2199b19c
Ed Schmalzle <Jonas714@gmail.com>
2008-11-11 12:33:12 -0500

    removed location unit test that applied to method that no longer exists

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -2,9 +2,4 @@ require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
   fixtures :locations
-  
-  def test_find_childless_locations
-    locations = Location.find_childless_locations
-    assert_equal( locations.size, 3 )
-  end
 end
