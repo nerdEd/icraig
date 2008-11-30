bd4b4f142695849c597cc50dd1454d69cbdf9f9c
Ed Schmalzle <Jonas714@gmail.com>
2008-11-30 15:32:36 -0500

    changed session clearing code in base_controller

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,10 +2,7 @@ class BaseController < ApplicationController
   
   def index
     @page_title = "iCraig Home"
-    session[ primary_location ] = nil
-    session[ sub_location ] = nil
-    session[ primary_category ] = nil
-    session[ sub_category ] = nil
+    session.clear
   end
   
 end
