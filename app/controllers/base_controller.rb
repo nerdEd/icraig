ecfceee95e57b2bd1682323b92dc40d1e8842ede
Ed Schmalzle <Jonas714@gmail.com>
2008-11-30 15:03:41 -0500

    updated home page controller to clear out users session

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,6 +2,10 @@ class BaseController < ApplicationController
   
   def index
     @page_title = "iCraig Home"
+    session[ primary_location ] = nil
+    session[ sub_location ] = nil
+    session[ primary_category ] = nil
+    session[ sub_category ] = nil
   end
   
 end
