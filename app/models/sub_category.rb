c4e3bbda552e5c7b5747e32a2bcc31afb6b2a3f9
Ed Schmalzle <Jonas714@gmail.com>
2008-11-30 22:54:43 -0500

    cleaned up models

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,7 +1,7 @@
 class SubCategory < Category
   has_and_belongs_to_many :primary_categories
   
-  def self.create_or_retrieve( name, code )
+  def SubCategory.create_or_retrieve( name, code )
     existing_sub_categories = SubCategory.find( :all, :conditions => [ "code = ?", code ] )
     if( existing_sub_categories.empty? ) then
       sub_category = SubCategory.new( :name => name, :code => code )
@@ -12,11 +12,11 @@ class SubCategory < Category
     end
   end
   
-  def self.category_anchors_from_doc( doc, category_name )
+  def SubCategory.category_anchors_from_doc( doc, category_name )
     return  ( doc/"table.w2[@summary='#{category_name}'] a")
   end
   
-  def self.create_from_anchor( anchor_element )
+  def SubCategory.create_from_anchor( anchor_element )
     SubCategory.create_or_retrieve( anchor_element.inner_html, anchor_element.attributes[ 'href' ] )     
   end
   
--- app/models/sub_location.rb
+++ app/models/sub_location.rb
@@ -1,13 +1,13 @@
 class SubLocation < Location
   belongs_to :primary_location
   
-  def self.create_from_anchor( anchor_element )
+  def SubLocation.create_from_anchor( anchor_element )
     name = anchor_element.inner_html.gsub( /<b>|<\/b>/i, '' )
     url = anchor_element.attributes[ 'href' ]
     return SubLocation.create( :name => name, :url => url )
   end
   
-  def self.location_anchors_from_doc( doc )
+  def SubLocation.location_anchors_from_doc( doc )
     return ( doc/"#list/a" )
   end
   
