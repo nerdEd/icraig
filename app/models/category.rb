8269d5da2df86be0764df7a073951ff4195197af
Ed Schmalzle <Jonas714@gmail.com>
2008-11-30 22:52:43 -0500

    added uniqueness validation for location and category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base  
-
+  validates_uniqueness_of :name, :code
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base  
-  has_and_belongs_to_many :primary_categories  
+  has_and_belongs_to_many :primary_categories
+  validates_uniqueness_of :name, :code, :url  
 end
