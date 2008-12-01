24920037cc6ce97a10b52f7bf921712ac1a38e8c
Ed Schmalzle <Jonas714@gmail.com>
2008-11-30 22:48:36 -0500

    clear out session when user enters base_controller

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,7 +2,10 @@ class BaseController < ApplicationController
   
   def index
     @page_title = "iCraig Home"
-    session.clear
+    session[ :location ] = nil
+    session[ :sub_location ] = nil
+    session[ :category ] = nil 
+    session[ :sub_category ] = nil
   end
   
 end
