8f061fc9bf52efff06fc4439138b24c1e100bde5
Ed Schmalzle <Jonas714@gmail.com>
2008-12-08 12:45:54 -0500

    cleaned up PrimaryCategoryTest

--- test/unit/primary_category_test.rb
+++ test/unit/primary_category_test.rb
@@ -1,13 +1,11 @@
 require 'test_helper'
 
 class PrimaryCategoryTest < ActiveSupport::TestCase
-  fixtures :categories
   
   def test_create_or_retrieve
-    primary_categories = Array.new
-    primary_categories << PrimaryCategory.create_or_retrieve( 'Junk', 'JunkSelling' )
-    primary_categories << PrimaryCategory.create_or_retrieve( 'Housing', 'house' )
-    assert_equal( primary_categories.size, 2 )
+    # A Primary Category 'Housing' 'house' has already been created by fixtures
+    PrimaryCategory.create_or_retrieve( 'Housing', 'house' )
+    assert_equal( 1, PrimaryCategory.find( :all ).size )
   end
   
   def test_category_anchors_from_doc
@@ -18,9 +16,10 @@ class PrimaryCategoryTest < ActiveSupport::TestCase
   end
   
   def test_create_from_anchor
+    number_of_categories_from_fixture = PrimaryCategory.find( :all ).size
     doc = open( File.dirname(__FILE__) + '/../fixtures/craigslist_sub_location.html' ) { |f| Hpricot(f) }
     anchor_elements = PrimaryCategory.category_anchors_from_doc( doc )
-    first_anchor = anchor_elements.first
+    first_anchor = anchor_elements.first    
     anchor_elements.each do | anchor |
       primary_category = PrimaryCategory.create_from_anchor( anchor )
       assert_not_nil( primary_category )
@@ -28,7 +27,8 @@ class PrimaryCategoryTest < ActiveSupport::TestCase
       assert_equal( anchor.attributes[ 'href' ], primary_category.code )
       assert_equal( anchor.inner_html, primary_category.name )
     end
-    assert_equal( 7, PrimaryCategory.find( :all ).size )
+    
+    assert_equal( 7 + number_of_categories_from_fixture , PrimaryCategory.find( :all ).size )
   end
   
 end
