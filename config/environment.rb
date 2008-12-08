1296178795fee4e830e76d8fbbcdf55d7f22a223
Ed Schmalzle <Jonas714@gmail.com>
2008-12-07 23:42:51 -0500

    configured hpricot gem version

--- config/environment.rb
+++ config/environment.rb
@@ -23,9 +23,9 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
   # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
