b15ab65a340978d37b74f9e19d787230a07912fa
Ed Schmalzle <Jonas714@gmail.com>
2008-12-08 12:46:44 -0500

    refactored SubCategory to take advanatage of ActiveRecord validations

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -2,13 +2,10 @@ class SubCategory < Category
   has_and_belongs_to_many :primary_categories
   
   def SubCategory.create_or_retrieve( name, code )
-    existing_sub_categories = SubCategory.find( :all, :conditions => [ "code = ?", code ] )
-    if( existing_sub_categories.empty? ) then
-      sub_category = SubCategory.new( :name => name, :code => code )
-      sub_category.save
-      return sub_category
-    else
-      sub_category = existing_sub_categories.first
+    begin
+      SubCategory.create( :name => name, :code => code )      
+    rescue
+      return SubCategory.find( :all, :conditions => [ "code = ?", code ] ).first
     end
   end
   
