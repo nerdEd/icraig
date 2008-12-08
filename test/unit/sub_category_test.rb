21ac9e51944dd893af8db81e524d2173c92511f7
Ed Schmalzle <Jonas714@gmail.com>
2008-12-08 12:46:04 -0500

    cleaned up SubCategoryTest

--- test/unit/sub_category_test.rb
+++ test/unit/sub_category_test.rb
@@ -1,13 +1,11 @@
 require 'test_helper'
 
 class SubCategoryTest < ActiveSupport::TestCase
-  fixtures :categories
   
   def test_create_or_retrieve
-    sub_categories = Array.new
-    sub_categories << SubCategory.create_or_retrieve( 'Junk', 'JunkSelling' )
-    sub_categories << SubCategory.create_or_retrieve( 'Apartments', 'apartment' )
-    assert_equal( sub_categories.size, 2 )
+    # category 'Apartments' 'apartment' already created by fixture
+    SubCategory.create_or_retrieve( 'Apartments', 'apartment' )
+    assert_equal( 1, SubCategory.find( :all ).size )
   end
   
   def test_category_anchors_from_doc
