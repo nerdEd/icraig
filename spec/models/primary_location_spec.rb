ad005855e8de72416c8a26d54b7841394b3ed418
Ed Schmalzle <Jonas714@gmail.com>
2009-04-01 16:51:40 -0400

    PrimaryLocation spec now uses should_not be_empty instead of checking to see if the first element of the list is nil

--- spec/models/primary_location_spec.rb
+++ spec/models/primary_location_spec.rb
@@ -8,13 +8,13 @@ describe PrimaryLocation do
   
   it "should find categories as children if it has no sub locations" do
     @primary_location.primary_categories << PrimaryCategory.new
-    @primary_location.children.first.should_not be_nil
+    @primary_location.children.should_not be_empty 
     @primary_location.children.first.should be_a( PrimaryCategory )
   end
   
   it "should find sub locations for children if it has sub locations" do
     @primary_location.sub_locations << SubLocation.new
-    @primary_location.children.first.should_not be_nil
+    @primary_location.children.should_not be_empty
     @primary_location.children.first.should be_a( SubLocation )
   end
   
