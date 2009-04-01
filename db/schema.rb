59ceebdfa9c125095ad51f5a038eaa1eaff1d379
Ed Schmalzle <Jonas714@gmail.com>
2009-04-01 16:52:24 -0400

    added skeleton of search model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081027180159) do
+ActiveRecord::Schema.define(:version => 20090401204119) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(:version => 20081027180159) do
   create_table "primary_categories_sub_categories", :id => false, :force => true do |t|
     t.integer "primary_category_id"
     t.integer "sub_category_id"
+  end
+
+  create_table "searches", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
