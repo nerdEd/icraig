401135d674044788a4f62e6a6194c5134dfac902
Ed Schmalzle <Jonas714@gmail.com>
2009-04-13 22:23:33 -0400

    got rid of long helper method calls that were determining resource paths in favor of url_for

--- app/helpers/primary_locations_helper.rb
+++ app/helpers/primary_locations_helper.rb
@@ -1,11 +1,3 @@
 module PrimaryLocationsHelper
   
-  def primary_location_child_url( location, child )
-    if( child.instance_of? SubLocation ) then
-      return primary_location_sub_location_url( location, child )
-    else
-      return primary_location_primary_category_url( location, child )
-    end
-  end
-  
 end
